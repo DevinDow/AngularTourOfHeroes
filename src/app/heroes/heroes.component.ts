a74c12565df61f2464396e5866c34baf08659f9a
Devin Dow <DevinDow@gmail.com>
2018-10-21 16:01:55 -0700

    fix

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
