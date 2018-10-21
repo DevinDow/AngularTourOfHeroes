233dc3f370164de0d3a8164b0b4d3caf47c37088
Devin Dow <DevinDow@gmail.com>
2018-10-21 09:09:02 -0700

    generate a new component with an Inline Template : "ng generate component devin -it", using ngForm

---
+++ src/app/devin/devin.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DevinComponent } from './devin.component';
+
+describe('DevinComponent', () => {
+  let component: DevinComponent;
+  let fixture: ComponentFixture<DevinComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DevinComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DevinComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
