be2e2df8ae019c51af96528fb05ca8007d9ee17b
Devin Dow <DevinDow@gmail.com>
2018-10-21 14:47:01 -0700

    implement Messages Component, Hero Service calls messagesService.add

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs'
+import { MessagesService } from "./messages.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
-    console.log('** HeroService.getHeroes()')
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
   
