fdc908a2bf6d2ba282abb2e13da95e9c15996938
Devin Dow <DevinDow@gmail.com>
2018-10-21 16:13:15 -0700

    implement HeroDetails using route :id

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -18,8 +18,15 @@ export class HeroDetailComponent implements OnInit {
     private heroService: HeroService,
     private location: Location
   ) {}
-  
-  ngOnInit() {
-  }
 
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+  
 }
