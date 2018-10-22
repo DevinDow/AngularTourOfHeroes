b2bc06b0ff1024fd438a6020825afa3b924b16b0
Devin Dow <DevinDow@gmail.com>
2018-10-22 08:24:09 -0700

    HeroesComponent.add() calls heroService.addHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -56,7 +56,15 @@ export class HeroService {
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     );
-  }  
+  }
+
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 
   /**
    * Handle Http operation that failed.
